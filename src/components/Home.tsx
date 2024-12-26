5e680c5f035106390a908245748c8351dcc88a64
Nimesh sajith kumara <sajithkumaranimesh@gmail.com>
2024-12-26 16:11:34 +0530

    change the navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ export const Home = () => {
         <div className="container col-xxl-8 px-4 py-5">
     <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
       <div className="col-10 col-sm-8 col-lg-6">
-        <img  className="d-block mx-lg-auto img-fluid" src={mainImage} alt="Main" width="500" height="500" loading="lazy"/>
+        <img  className="d-block mx-lg-auto img-fluid" src={mainImage} alt="Main" width="600" height="600" loading="lazy"/>
       </div>
       <div className="col-lg-6">
         <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
