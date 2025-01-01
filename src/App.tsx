90bd4e9585d7c8ae85e814307a3a174793ac55ed
sajithkumaranimesh <sajithkumaranimesh@gmail.com>
2025-01-01 14:36:09 +0530

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { Card } from "./components/Card"
-import { Home } from "./components/Home"
-import { Menu } from "./components/Menu"
-import { Navbar } from "./components/Navbar"
 
 function App() {
 
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Menu/>
-    <Card/>
+    <p>Hello world</p>
     </>
   )
 }
